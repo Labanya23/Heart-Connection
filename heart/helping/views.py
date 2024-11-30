4420f598457c64372f3cb422f59f156a3cf38f0d
Labanya23 <100686917+Labanya23@users.noreply.github.com>
2024-11-30 16:52:53 +0600

    views update

--- heart/helping/views.py
+++ heart/helping/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views import View
+from .models import Donor,Volunteer,Donation,DonationArea,Gallery
+from .forms import UserForm,DonorSignupForm,VolunteerSignupForm,LoginForm,MyPasswordChangeForm,DonateNowForm,DonationAreaForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from datetime import date
 
 # Create your views here.
 def index(request):

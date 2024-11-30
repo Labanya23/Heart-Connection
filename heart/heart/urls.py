d5aab1ad7e3dbda63c537cd9ab03c6c84338c606
Tamanna Toma <tamannatoma208542@gmail.com>
2024-11-30 17:07:11 +0600

    urls update

--- heart/heart/urls.py
+++ heart/heart/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from helping import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from helping.forms import MyPasswordResetForm,MySetPasswordForm
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
